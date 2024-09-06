14c08a3697b72195ac13707be0ba88060720fc39
odes <odehse@mail.uc.edu>
2024-09-06 11:05:43 -0400

    Update to PS-2 hw

---
+++ ps-2/Newman_Exercise_2.9.py
@@ -0,0 +1,7 @@
+# Write a program to calculate and print the Madelung constant for sodium chloride. Use as large a value of L as you can, while still having your program run in reasonable time- say in a minute or less. Note that the physical constants drop out so you do not need to worry about them. Write two versions of the code, one which uses a for loop and one which does not. USe %timeit to determine which one is faster. 
+
+# VERSION ONE: WITH A FOR LOOP
+
+# We need to define the Madelung constant
+
+# VERSION TWO: WITHOUT A FOR LOOP
