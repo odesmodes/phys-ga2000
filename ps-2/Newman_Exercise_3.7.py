f86351c77aae930ac0a9569fddf14ce208f2d54c
odesmodes <odesmodes@WOPR.mynetworksettings.com>
2024-09-14 18:31:27 -0400

    Finished Q3

---
+++ ps-2/Newman_Exercise_3.7.py
@@ -0,0 +1,2 @@
+# Write a program to make an image of the Mandelbrot set by performing the iteration for all values of c = x + iy on a N x N grid spanning the region where -2 <= x/y <= 2. Make a density plot in which grid points inside the Mandelbrot set are colored black and those outside are colored white.  
+
