0d9cc824f7e0ef49050944cd888076b33f49198a
odesmodes <odesmodes@WOPR.mynetworksettings.com>
2024-09-27 12:11:17 -0400

    Fixed Q1 part a/b

---
+++ ps-4/Newman5.13.py
@@ -0,0 +1,38 @@
+import numpy as np
+import math as m
+from matplotlib import pyplot as plt
+
+def H(x,n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 2*x
+    H0, H1 = 1, 2 * x
+    for i in range(2, n+1):
+        Hn = 2 * x * H1 - 2 * (i-1) * H0
+        H0, H1 = H1, Hn
+    return H1
+
+def psi(x,n):
+    return 1/(np.sqrt(2**n*m.factorial(n)*np.sqrt(np.pi)))*np.e**(-x**2/2)*H(x,n)
+
+n_range = np.arange(0,3,1);
+x_range = np.linspace(-4,4,1000);
+
+figure, axis = plt.subplots(2,1)
+
+for i in n_range:
+    n_res = []
+    for j in x_range:
+        n_res.append(psi(j,i))
+    axis[0].plot(x_range,n_res)
+    axis[0].set_title("n range 0 to 4")
+
+n = 30
+x_range2 = np.linspace(-10,10,1000)
+
+n_res2 = [psi(i,n) for i in x_range2]
+
+axis[1].plot(x_range2,n_res2)
+
+plt.show()
---
+++ ps-4/Newman5.13_2.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math as m
+from gaussxw import gaussxw, gaussxwab
+
+def H(x,n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 2*x
+    H0, H1 = 1, 2 * x
+    for i in range(2, n+1):
+        Hn = 2 * x * H1 - 2 * (i-1) * H0
+        H0, H1 = H1, Hn
+    return H1
+
+def psi(x,n):
+    return 1/(np.sqrt(2**n*m.factorial(n)*np.sqrt(np.pi)))*np.e**(-x**2/2)*H(x,n)
+
+def cov(z,n):
+    linear = ((z/(1-z**2))**2)*(1+z**2)/((1-z**2)**2) #x**2 * the part that pops out
+    f = psi((z/(1-z**2)),n) 
+    return linear*np.abs(f)**2
+
+#def cov(z,n): # this one doesn't work for some reason
+#    linear = np.tan(z)**2
+#   f = psi(np.tan(z),n)/np.cos(z)**2
+#    return linear * np.abs(f)**2
+
+N = 100
+n = 5
+a = -1
+b = 1
+
+x,w = gaussxwab(N,a,b)
+
+s=0
+for k in range(N):
+    s+=w[k]*cov(x[k],n)
+print(np.sqrt(s))
+    
