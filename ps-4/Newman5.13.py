76123338087444a9d68022a8ad22658738e7c49d
odesmodes <odesmodes@WOPR.mynetworksettings.com>
2024-09-27 15:27:45 -0400

    Q3 part a and b

--- ps-4/Newman5.13.py
+++ ps-4/Newman5.13.py
@@ -2,6 +2,7 @@ import numpy as np
 import math as m
 from matplotlib import pyplot as plt
 
+# part a 
 def H(x,n):
     if n == 0:
         return 1
@@ -16,7 +17,7 @@ def H(x,n):
 def psi(x,n):
     return 1/(np.sqrt(2**n*m.factorial(n)*np.sqrt(np.pi)))*np.e**(-x**2/2)*H(x,n)
 
-n_range = np.arange(0,3,1);
+n_range = np.arange(0,4,1);
 x_range = np.linspace(-4,4,1000);
 
 figure, axis = plt.subplots(2,1)
@@ -28,6 +29,7 @@ for i in n_range:
     axis[0].plot(x_range,n_res)
     axis[0].set_title("n range 0 to 4")
 
+# Part b
 n = 30
 x_range2 = np.linspace(-10,10,1000)
 
@@ -35,4 +37,9 @@ n_res2 = [psi(i,n) for i in x_range2]
 
 axis[1].plot(x_range2,n_res2)
 
+plt.xlabel('x [m]')
+axis[0].set_ylabel('Psi_n(x)')
+axis[1].set_ylabel('Psi_30(x)')
+axis[0].legend(n_range, loc=1)
+plt.savefig('Q3partab.png')
 plt.show()
