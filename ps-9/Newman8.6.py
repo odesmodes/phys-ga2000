20668696ee46bedd49d1b6d977ecdbc7478b762a
odesmodes <odesmodes@WOPR.mynetworksettings.com>
2024-11-22 11:06:56 -0500

    Added ps-9

---
+++ ps-9/Newman8.6.py
@@ -0,0 +1,153 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#for A
+def f(r,t):
+    x = r[0]
+    dx = r[1]
+    fx = dx
+    fdx = -omega**2*x
+    return np.array([fx,fdx],float)
+
+#for C
+def f2(r,t):
+    x = r[0]
+    dx = r[1]
+    fx = dx
+    fdx = -omega**2*x**3
+    return np.array([fx,fdx],float)
+
+#for E
+def f3(r,t):
+    x = r[0]
+    dx = r[1]
+    fx = dx
+    fdx = -omega**2*x + mu*(1-x**2)*dx
+    return np.array([fx,fdx],float)
+
+#for A,B
+def calculate(tpoints, r, h):
+    xpoints = []
+    ypoints = []
+    for t in tpoints:
+        xpoints.append(r[0])
+        ypoints.append(r[1])
+        k1 = h*f(r,t)
+        k2 = h*f(r+0.5*k1,t+0.5*h)
+        k3 = h*f(r+0.5*k2,t+0.5*h)
+        k4 = h*f(r+k3,t+h)
+        r += (k1+2*k2+2*k3+k4)/6
+    return (xpoints, ypoints)
+
+#for C
+def calculate2(tpoints, r, h):
+    xpoints = []
+    ypoints = []
+    for t in tpoints:
+        xpoints.append(r[0])
+        ypoints.append(r[1])
+        k1 = h*f2(r,t)
+        k2 = h*f2(r+0.5*k1,t+0.5*h)
+        k3 = h*f2(r+0.5*k2,t+0.5*h)
+        k4 = h*f2(r+k3,t+h)
+        r += (k1+2*k2+2*k3+k4)/6
+    return (xpoints, ypoints)
+
+#E
+def calculate3(tpoints, r, h):
+    xpoints = []
+    ypoints = []
+    for t in tpoints:
+        xpoints.append(r[0])
+        ypoints.append(r[1])
+        k1 = h*f3(r,t)
+        k2 = h*f3(r+0.5*k1,t+0.5*h)
+        k3 = h*f3(r+0.5*k2,t+0.5*h)
+        k4 = h*f3(r+k3,t+h)
+        r += (k1+2*k2+2*k3+k4)/6
+    return (xpoints, ypoints)
+
+#part A
+
+omega = 1
+
+a = 0
+b = 50
+N = 1000
+h = (b-a)/N
+
+tpoints = np.arange(a,b,h)
+r = np.array([1.0,0.0],float)
+xpoints, ypoints = calculate(tpoints, r, h)
+
+plt.title("Second order: x = 1")
+plt.xlabel("time (t)")
+plt.ylabel("Amplitude (y)")
+plt.plot(tpoints,xpoints)
+plt.show()
+plt.close()
+
+#part B
+
+r = np.array([2.0,0.0],float)
+xpoints, ypoints = calculate(tpoints, r, h)
+
+plt.title("Second order: x = 2")
+plt.xlabel("time (t)")
+plt.ylabel("Amplitude (y)")
+plt.plot(tpoints,xpoints)
+plt.show()
+plt.close()
+
+
+r = np.array([1.0,0.0],float)
+xpoints, ypoints = calculate2(tpoints, r, h)
+
+plt.title("Second order, x^3: x = 1")
+plt.xlabel("time (t)")
+plt.ylabel("Amplitude (y)")
+plt.plot(tpoints,xpoints)
+plt.show()
+plt.close()
+
+r = np.array([2.0,0.0],float)
+xpoints, ypoints = calculate2(tpoints, r, h)
+
+plt.title("Second order, x^3: x = 2")
+plt.xlabel("time (t)")
+plt.ylabel("Amplitude (y)")
+plt.plot(tpoints,xpoints)
+plt.show()
+plt.close()
+
+
+#part D
+
+plt.title("Second order, x^3: Phase Space")
+plt.xlabel("x")
+plt.ylabel("dx/dt")
+plt.plot(xpoints,ypoints)
+plt.show()
+plt.close()
+
+#part E
+
+mu = 1
+
+a = 0
+b = 20
+N = 1000
+h = (b-a)/N
+
+tpoints = np.arange(a,b,h)
+for mu in [1,2,4]:
+    r = np.array([1.0,0.0],float)
+    xpoints, ypoints = calculate3(tpoints, r, h)
+    plt.title(f"Van der Pol Phase Space: mu = {mu}")
+    plt.xlabel("x")
+    plt.ylabel("dx/dt")
+    plt.plot(xpoints,ypoints)
+    plt.show()
+    plt.close()
+
---
+++ ps-9/Newman8.7.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = .5
+R = 8/100
+v0 = 100
+rho = 1.22
+C = 0.47
+g = 9.81
+
+theta = np.radians(30)
+
+a = 0
+b = 20
+N = 1000
+h = (b-a)/N
+
+tpoints = np.arange(a,b,h)
+
+def f(r, t, m = 0.5):
+    x, y, dx, dy = r
+    v = np.sqrt(dx**2 + dy**2)
+    F_drag = 0.5 * np.pi * R**2 * rho * C * v**2
+    ddx = -F_drag/m * dx/v
+    ddy = -F_drag/m * dy/v - g
+    return np.array([dx,dy,ddx,ddy],float)
+
+def calculate(tpoints, r, h,m=0.5):
+    xpoints=[]
+    ypoints=[]
+    for t in tpoints:
+        xpoints.append(r[0])
+        ypoints.append(r[1])
+        k1=h*f(r,t,m)
+        k2=h*f(r+0.5*k1,t+0.5*h,m)
+        k3=h*f(r+0.5*k2,t+0.5*h,m)
+        k4=h*f(r+k3,t+h,m)
+        r += (k1+2*k2+2*k3+k4)/6
+        if r[1]<0:
+            break
+    return (xpoints, ypoints)
+
+r = [0.0, 0.0, v0*np.cos(theta), v0*np.sin(theta)]
+
+xpoints,ypoints=calculate(tpoints,r,h,m=1)
+plt.plot(xpoints,ypoints)
+plt.title("Cannonball Trajectory")
+plt.xlabel("x (m)")
+plt.ylabel("y (m)")
+plt.show()
+plt.close()
+
+res = [calculate(tpoints,r,h,m) for m in np.arange(0.5,5.5,0.5)]
+for i,m in zip(res,np.arange(0.5,5.5,0.5)):
+    plt.plot(i[0],i[1], label=f"Mass = {m} kg")
+
+plt.title("Cannonball Trajectories")
+plt.xlabel("x (m)")
+plt.ylabel("y (m)")
+plt.legend(loc='upper left')
+plt.show()
